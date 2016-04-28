aff5dcd322b735d3f1f49900de14ae9038479ff5
justinyangusa <justin@y4ng.com>
2016-04-27 20:02:06 -0700

    Heroku works now sort of

--- app.py
+++ app.py
@@ -100,4 +100,5 @@ def classify_upload():
   return send_file(resultFilename)
 
 if __name__ == '__main__':
-  app.run(debug=True)
+  port = int(os.environ.get("PORT", 5000))
+  app.run(host='0.0.0.0', port=port)
